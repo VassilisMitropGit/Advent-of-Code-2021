ef2a14e13ac67dbf28faeb51e5e51fb5776857ff
GMX\000307726 <vassilis.mitropoulos@ibm.com>
2021-12-06 16:20:18 +0200

    Day2 solution

--- Day2/Dive.py
+++ Day2/Dive.py
@@ -1,3 +1,29 @@
 f = open("Dive.txt", "r")
 lines = f.readlines()
 f.close()
+horizontal = 0
+#This is the depth used for Part1
+depth1 = 0
+#This is the depth used for Part2
+depth2 = 0
+aim = 0
+
+for line in lines:
+    command = line.split(" ")[0]
+    units = int(line.split(" ")[1])
+    if command == 'forward': 
+        horizontal += units
+        depth2 += aim*units
+    elif command == 'down': 
+        depth1 += units
+        aim += units
+    else: 
+        depth1 -= units
+        aim -= units
+
+print('My final position for Part1 is:')
+print('Horizontal: ',horizontal,' Depth: ',depth1)
+print('Part 1 solution: ',horizontal*depth1)
+print('My final position for Part2 is:')
+print('Horizontal: ',horizontal,' Depth: ',depth2)
+print('Part 2 solution: ',horizontal*depth2)
